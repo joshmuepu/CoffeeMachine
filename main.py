c70d201a7c9521c61dee147f8d0e8adcf3ee3f7d
Josh Muepu <josh.muepu@swimlane.com>
2023-01-01 00:15:45 -0500

    fixed money increase and made program more modular. Completed project.

--- main.py
+++ main.py
@@ -11,12 +11,34 @@ def report():
     print(f"Money: ${money}")
 
 
+def add_money(drink):
+    global money
+    money += MENU[drink]["cost"]
+
+
+def check_money(drink):
+    cost = MENU[drink]["cost"]
+    print("Please, insert coins.")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How may pennies?: "))
+    total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+    if total < cost:
+        print("Sorry, that's not enough money.")
+    else:
+        change = total - cost
+        print(f"Here is ${change} in change")
+        make_drink(drink)
+
+
 def make_espresso():
     if resources["water"] < 50:
         print("Sorry, there is not enough water")
     elif resources["coffee"] < 18:
         print("Sorry, there is not enough coffee")
     else:
+        add_money("espresso")
         resources["water"] -= 50
         resources["coffee"] -= 18
         print("Here is your espresso. Enjoy")
@@ -30,6 +52,7 @@ def make_latte():
     elif resources["milk"] < 150:
         print("Sorry, there is not enough coffee")
     else:
+        add_money("latte")
         resources["water"] -= 200
         resources["coffee"] -= 24
         resources["milk"] -= 150
@@ -44,33 +67,33 @@ def make_cappuccino():
     elif resources["milk"] < 100:
         print("Sorry, there is not enough coffee")
     else:
+        add_money("cappuccino")
         resources["water"] -= 250
         resources["coffee"] -= 24
         resources["milk"] -= 100
         print("Here is your cappuccino. Enjoy")
 
 
+def make_drink(drink):
+    if drink == "espresso":
+        make_espresso()
+    elif drink == "latte":
+        make_latte()
+    elif drink == "cappuccino":
+        make_cappuccino()
+    else:
+        print("Wrong selection")
+
+
 while turn_on:
     user_choice = input("What would you like? (espresso/latte/cappuccino): ")
 
-    if user_choice == "report":
-        report()
-    elif user_choice == "espresso":
-        make_espresso()
-        money += 1.5
-    elif user_choice == "latte":
-        make_latte()
-        money += 2.5
-    elif user_choice == "cappuccino":
-        make_cappuccino()
-        money += 3.0
-    elif user_choice == "off":
+    if user_choice == "off":
         print("Good Bye!!")
         turn_on = False
+    elif user_choice == "report":
+        report()
     else:
-        print("Wrong selection.")
-        turn_on = False
-
-
+        check_money(user_choice)
 
 
